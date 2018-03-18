86edd39d96c86e64d287282c3033294ac7ce8a7a
VanAnhTo <vananhto.bg@gmail.com>
2018-03-18 22:16:28 +0700

    Fix warning

--- seleniumeasy/src/basic/calendar.java
+++ seleniumeasy/src/basic/calendar.java
@@ -1,10 +1,10 @@
 package basic;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import java.util.List;
+//import java.util.List;
 public class calendar {
 
 	public static void main(String[] args) throws InterruptedException {
@@ -22,7 +22,7 @@ public class calendar {
 		}
 		
 		
-		List<WebElement> dates = driver.findElements(By.cssSelector(".day"));
+		//List<WebElement> dates = driver.findElements(By.cssSelector(".day"));
 		int count = driver.findElements(By.cssSelector(".day")).size(); 
 		for (int i = 0; i < count; i++){ 
 			String text = driver.findElements(By.cssSelector(".day")).get(i).getText();
